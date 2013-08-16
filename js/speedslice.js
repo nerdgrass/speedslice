1913d0aad1b8f844708771e715fcce2e62e4fca3
William Neely <william@piecewise.com>
2013-08-16 13:04:25 -0400

    Menu finally fixed

--- js/speedslice.js
+++ js/speedslice.js
@@ -1,3 +1,70 @@
+google.ui.FastButton = function(element, handler) {
+  this.element = element;
+  this.handler = handler;
+
+  element.addEventListener('touchstart', this, false);
+  element.addEventListener('click', this, false);
+};
+
+google.ui.FastButton.prototype.handleEvent = function(event) {
+  switch (event.type) {
+    case 'touchstart': this.onTouchStart(event); break;
+    case 'touchmove': this.onTouchMove(event); break;
+    case 'touchend': this.onClick(event); break;
+    case 'click': this.onClick(event); break;
+  }
+};
+google.ui.FastButton.prototype.onTouchStart = function(event) {
+  event.stopPropagation();
+
+  this.element.addEventListener('touchend', this, false);
+  document.body.addEventListener('touchmove', this, false);
+
+  this.startX = event.touches[0].clientX;
+  this.startY = event.touches[0].clientY;
+};
+google.ui.FastButton.prototype.onTouchMove = function(event) {
+  if (Math.abs(event.touches[0].clientX - this.startX) > 10 ||
+      Math.abs(event.touches[0].clientY - this.startY) > 10) {
+    this.reset();
+  }
+};
+google.ui.FastButton.prototype.onClick = function(event) {
+  event.stopPropagation();
+  this.reset();
+  this.handler(event);
+
+  if (event.type == 'touchend') {
+    google.clickbuster.preventGhostClick(this.startX, this.startY);
+  }
+};
+
+google.ui.FastButton.prototype.reset = function() {
+  this.element.removeEventListener('touchend', this, false);
+  document.body.removeEventListener('touchmove', this, false);
+};
+google.clickbuster.preventGhostClick = function(x, y) {
+  google.clickbuster.coordinates.push(x, y);
+  window.setTimeout(google.clickbuster.pop, 2500);
+};
+
+google.clickbuster.pop = function() {
+  google.clickbuster.coordinates.splice(0, 2);
+};
+google.clickbuster.onClick = function(event) {
+  for (var i = 0; i < google.clickbuster.coordinates.length; i += 2) {
+    var x = google.clickbuster.coordinates[i];
+    var y = google.clickbuster.coordinates[i + 1];
+    if (Math.abs(event.clientX - x) < 25 && Math.abs(event.clientY - y) < 25) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+};
+
+document.addEventListener('click', google.clickbuster.onClick, true);
+google.clickbuster.coordinates = [];
+
 address=new Object();
 address.addrNick="";
 address.addr="";
@@ -326,7 +393,11 @@ function loadInfo(){
 			makeActive("#deliveryLoc>.infoWrapper",blockChanges);
 			$("#deleteAddress").show();
 		});
+	google.ui.FastButton(document.getElementsByClassName("home")[0],function(){
+		switchSlides(3);
 	});
+	});
+	
 }
 function makeActive(cntnrStr,rdOnlyStr){
 	$(rdOnlyStr).removeAttr("readonly");
@@ -1012,7 +1083,7 @@ function adjustSlider(iContMrgnTop,innerContainer,sliderHandle,sliderHeight){
 	$(sliderHandle).css("top",slidePixels+"px"); 
 }
 function onMenuKeyDown(){
-	$('nav#my-menu').mmenu().trigger("open.mm");
+	$('nav#my-menu').mmenu().trigger($('nav#my-menu:visible').length==0 ? "open.mm":"close.mm");
 }
 function onBackButton(){
 	if(lastSlides.length!=0){
